98fe2f82f01e48e9659e6e0a91f16ec090d6e674
yorupy <198967142+yorupy@users.noreply.github.com>
2025-03-15 14:14:26 -0500

    Create card elements

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ function addBookToLibrary(name, author, description, chapters, read, cover) {
 }
 
 
+function createBookCard(name, author, description, chapters, read, cover) {
+    const card = document.createElement("article");
+    card.classList.add(".card");
+
+    const titleHeading = document.createElement("h3");
+    titleHeading.textContent = name;
+    titleHeading.classList.add("title");
+
+    const authorParagraph = document.createElement("p");
+    authorParagraph.textContent = author;
+    authorParagraph.classList.add("author");
+
+    const chaptersParagraph = document.querySelector("p");
+    chaptersParagraph.textContent = chapters;
+    chaptersParagraph.classList.add("chapters");
+
+    const descriptionParagraph = document.querySelector("p");
+    descriptionParagraph.textContent = description;
+    descriptionParagraph.classList.add("description");
+
+}
+
 
 addBookToLibrary("I Want To End This Love Game", "Doumoto Yuuki", 'Childhood friends who have been together since they were little. They both realize their feelings for each other, but are too close to be honest. What connects them is the "I love you game" that they have been playing since they were little. The end of their love is decided in this simple game where the winner is the one who makes the other embarrassed!', 80, false, "https://mangadex.org/covers/acdbf57f-bf54-41b4-8d92-b3f3d14c852e/2e239897-0808-4d2f-bc05-9e12b36b361f.jpg");
 addBookToLibrary("The Dangers In My Heart", "Sakura Norio", "Following Ichikawa Kyoutarou, a teenage boy belonging to the very bottom caste of his school, this work details his troubled interactions with a certain classmate, and his attempt to hide murderous impulses that find themselves in the darkest recesses of his soul.", 121, false, "https://mangadex.org/covers/3df1a9a3-a1be-47a3-9e90-9b3e55b1d0ac/60b13fdd-6541-48e5-9181-fe1903d840f5.jpg");
