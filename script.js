f1182813b91d6a05aaedb38f679d31038af33d2e
yorupy <198967142+yorupy@users.noreply.github.com>
2025-03-15 14:34:48 -0500

    Create manga cards

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(name, author, chapters, read, cover) {
+function Book(name, author, description, chapters, read, cover) {
     if (!new.target) {
         throw Error("Must use the new operator to call the function")
     }
     this.name = name;
+    this.description = description;
     this.author = author;
     this.chapters = chapters;
     this.read = read;
@@ -20,7 +21,13 @@ function addBookToLibrary(name, author, description, chapters, read, cover) {
 
 function createBookCard(name, author, description, chapters, read, cover) {
     const card = document.createElement("article");
-    card.classList.add(".card");
+    card.classList.add("card");
+
+    const coverImage = document.createElement("img");
+    coverImage.src = cover;
+
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("content");
 
     const titleHeading = document.createElement("h3");
     titleHeading.textContent = name;
@@ -28,21 +35,23 @@ function createBookCard(name, author, description, chapters, read, cover) {
 
     const info = createCardInfo(author, chapters);
 
-    const descriptionParagraph = document.querySelector("p");
+    const descriptionParagraph = document.createElement("p");
     descriptionParagraph.textContent = description;
     descriptionParagraph.classList.add("description");
 
-    card.append(titleHeading, info, descriptionParagraph);
+    contentContainer.append(titleHeading, info, descriptionParagraph);
+
+    card.append(coverImage, contentContainer);
 
     return card;
 
 }
 
 function createCardInfo(author, chapters) {
-    const infoContainer = document.createElement(".div");
+    const infoContainer = document.createElement("div");
     infoContainer.classList.add("info");
 
-    const authorContainer = document.createElement(".div");
+    const authorContainer = document.createElement("div");
     authorContainer.classList.add("author-container");
     const authorParagraph = document.createElement("p");
     authorParagraph.textContent = author;
@@ -50,9 +59,9 @@ function createCardInfo(author, chapters) {
     const authorIcon = createIconImage("./assets/user.svg", 24);
     authorContainer.append(authorParagraph, authorIcon);
 
-    const chaptersContainer = document.createElement(".div");
+    const chaptersContainer = document.createElement("div");
     chaptersContainer.classList.add("chapters-container");
-    const chaptersParagraph = document.querySelector("p");
+    const chaptersParagraph = document.createElement("p");
     chaptersParagraph.textContent = chapters;
     chaptersParagraph.classList.add("chapters");
     const chaptersIcon = createIconImage("./assets/hash.svg", 24);
@@ -60,14 +69,24 @@ function createCardInfo(author, chapters) {
 
     infoContainer.append(authorContainer, chaptersContainer);
 
+    return infoContainer;
 }
 
 function createIconImage(source, size) {
     const icon = document.createElement("img");
-    img.src = source;
-    img.width = size;
-    img.height = size;
+    icon.src = source;
+    icon.width = size;
+    icon.height = size;
     return icon;
+}
+
+function appendBooksToDOM() {
+    const container = document.querySelector(".cards");
+    for (const book of myLibrary) {
+        const card = createBookCard(book.name, book.author, book.description, book.chapters, book.read, book.cover);
+        console.log(book);
+        container.append(card);
+    }
 }
 
 
@@ -75,3 +94,5 @@ addBookToLibrary("I Want To End This Love Game", "Doumoto Yuuki", 'Childhood fri
 addBookToLibrary("The Dangers In My Heart", "Sakura Norio", "Following Ichikawa Kyoutarou, a teenage boy belonging to the very bottom caste of his school, this work details his troubled interactions with a certain classmate, and his attempt to hide murderous impulses that find themselves in the darkest recesses of his soul.", 121, false, "https://mangadex.org/covers/3df1a9a3-a1be-47a3-9e90-9b3e55b1d0ac/60b13fdd-6541-48e5-9181-fe1903d840f5.jpg");
 addBookToLibrary("Secrets Of The Gal Wife", "Kudu", "Fuyuki is a beautiful and cool gal! But there's a secret side of her that she only shows in front of her husband...?", 66, true, "https://mangadex.org/covers/a920060c-7e39-4ac1-980c-f0e605a40ae4/07d02b26-cbd0-4323-8774-9d83579863d5.jpg");
 addBookToLibrary("The Angel Next Door Spoils Me Rotten", "Saeki-san", "Mahiru is a beautiful girl whose classmates all call her an “angel.” Not only is she a star athlete with perfect grades—she’s also drop-dead gorgeous. Amane‚ an average guy and self-admitted slob‚ has never thought much of the divine beauty‚ despite attending the same school. Everything changes‚ however‚ when he happens to see Mahiru sitting alone in a park during a rainstorm. Thus begins the strange relationship between this incredibly unlikely pair!", 20, true, "https://mangadex.org/covers/2fec6641-beb1-4ae9-a32f-204dd06688ac/17ec7cbc-0e37-4ec8-ad93-5c12ed742f91.jpg");
+
+appendBooksToDOM();
